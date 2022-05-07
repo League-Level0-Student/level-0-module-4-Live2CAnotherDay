5dbfb701895fa97029fc83501acf46d37d835307
league <league@imac-24.attlocal.net>
2022-05-06 18:27:01 -0700

    Finish the Fruit Quiz thingie

--- src/_99_extra/FruitQuiz.java
+++ src/_99_extra/FruitQuiz.java
@@ -13,15 +13,18 @@ public class FruitQuiz extends KeyAdapter {
 	void makeQuestions() {
 		question1 = new JLabel("<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
 		// 11. Make another question called "question2".  Use question1 above as a guide.
+		question2 = new JLabel("<html>Which is not a real vegetable? <br>A: Carrot <br> B: Tomato <br> C: Onion</html>" );
 	}
 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
-
+			System.out.println(keyCode);
 		// 2. Make 3 int variables that hold the key codes for A, b, and C
-		
+			int aVariable = 65;
+			int bVariable = 66;
+			int cVariable = 67;
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
 		
 		// 12. If question2 is showing,
@@ -31,7 +34,12 @@ public class FruitQuiz extends KeyAdapter {
 			
 		if (question1.isShowing()) {
 			// 3. If they selected the right fruit, do steps 4 and 7
-			
+			if (JLabel = cVariable){
+				correct();
+				nextQuestion(question2);
+			}else {
+				incorrect()
+			}
 				// 4. Call the correct() method
 				
 				// 7. Use the nextQuestion() method to go to question2
@@ -50,13 +58,13 @@ public class FruitQuiz extends KeyAdapter {
 		// the 'extra' package. It must be a .wav file. 
 		// There are lots on freesound.org
 		// 6. Use the playSound method to play your sound
-
+		playSound("correct.wav");
 
 	}
 
 	private void incorrect() {
 		// 10. Find a sound for wrong answers and put it in the default package. Use the playSound method to play it.
-
+		playSound("wrong.wav");
 	}
 
 	private void nextQuestion(JLabel newQuestion) {
